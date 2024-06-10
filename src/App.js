10a85a6d2a4b2917ae52de88cb46129f84329f84
mdonatoc <milagros.donato@fajardo.inter.edu>
2024-06-10 14:54:29 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,12 @@ const App = () => {
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
                         {
-                            /* Add Budget component here */
+                            
+                        /* Add Budget component here */
+                                          // Budget component
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
                         }        
 
                         {
